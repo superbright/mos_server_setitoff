f9a00ff37b36ec8c2419017b429f498ec6f3cd7c
Laura Mun <laurasumun@gmail.com>
2017-10-05 14:11:01 -0400

    split app into two pages and added additional functionalities
    - moved vibrator buttons underneath each player and separated buttons for each player
    - displayed JSON data on page

--- src/api.js
+++ src/api.js
@@ -15,6 +15,22 @@ api.get('/',
   }
 );
 
+api.get('/qa',
+  async (ctx, next) => {
+    await ctx.render('qa', {
+     user: 'museum'
+   });
+  }
+);
+
+api.get('/guest',
+  async (ctx, next) => {
+    await ctx.render('guest', {
+     user: 'museum'
+   });
+  }
+);
+
 api.post('/config', async (ctx, next) => {
       ctx.config.PLAYERS[ctx.request.body.player].ison = ctx.request.body.ison;
       ctx.body = JSON.stringify(ctx.request.body);
